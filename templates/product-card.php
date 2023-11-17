fd08e96cf105c0e04bbd2ccc3e658b24c23334df
Jawad Malik <82345120+jawadhere@users.noreply.github.com>
2023-11-17 22:29:47 +0500

    improve: Escaping and translation enhancements in product-card

--- templates/product-card.php
+++ templates/product-card.php
@@ -1,46 +1,48 @@
 <?php
-
 // Exit if accessed directly
 defined( 'ABSPATH' ) || exit;
 
-$cart = WC()->cart->get_cart();
+$cart          = WC()->cart->get_cart();
 
-$item = $cart[ $cart_item_key ];
+// Ensure $cart_item_key is set
+$cart_item_key = isset( $cart_item_key ) ? $cart_item_key : '';
 
-$product = $item['data'];
+$item          = isset( $cart[ $cart_item_key ] ) ? $cart[ $cart_item_key ] : array();
+$product_id    = isset( $item['product_id'] ) ? $item['product_id'] : 0;
 
-$thumbnail = $product->get_image();
+$product       = wc_get_product( $product_id );
 
-$thumbnail_url = esc_url( get_the_post_thumbnail_url( $product->id ) );
+// Check if the product exist
 
-$background_image = 'style=background-image:url("' . $thumbnail_url . '")';
+if( $product ) {
 
-$product_name = $product->get_title();
+	$thumbnail        = $product->get_image();
 
-$product_price = WC()->cart->get_product_price( $product );
+	$thumbnail_url    = esc_url( get_the_post_thumbnail_url( $product->id ) );
 
+	$background_image =  'style=background-image:url("' . $thumbnail_url . '")';
+
+	$product_name     =  $product->get_title();
+
+	$product_price = strip_tags( wc_price( $product->get_price() ) );
+} else {
+	$thumbnail = $thumbnail_url = $background_image = $product_name = $product_price = '';
+}
 ?>
 
-<div class="woosupercharge-modal-content-container" <?php echo $listview === 'list' ? $background_image : ''; ?> >
-  <h2><?php __( 'Added to Cart', 'woosupercharge') ?>Added to Cart</h2>
+<div class="woosupercharge-modal-content-container" <?php echo $listview === 'list' ? esc_attr( $background_image ) : ''; ?>>
+  <h2><?php _e( 'Added to Cart', 'woosupercharge' ); ?></h2>
   <div class="woosupercharge-modal-body">
-	<?php if ( $listview !== 'list' ) { ?>
-	<div class="woosupercharge-modal-image">
-		<?php echo $thumbnail; ?>
-	</div>
-	<?php } ?>
+    <?php if ( $listview !== 'list' && $thumbnail ) { ?>
+    <div class="woosupercharge-modal-image">
+      <?php echo $thumbnail; ?>
+    </div>
+    <?php } ?>
 
-	<div class="woosupercharge-modal-details">
-	  <h6> <?php echo esc_html( $product_name ); ?> </h6>
-	  <p> <?php  echo $product_price; ?> </p>
-	</div>
+    <div class="woosupercharge-modal-details">
+      <h6><?php echo esc_html( $product_name ); ?></h6>
+      <p><?php  echo  esc_html( $product_price ); ?></p>
+    </div>
   </div>
-  <a class="view-cart-btn" href="<?php echo wc_get_cart_url(); ?>">View Cart</a>
+  <a class="view-cart-btn" href="<?php echo esc_url( wc_get_cart_url() ); ?>"><?php esc_html_e( 'View Cart', 'woosupercharge' ); ?></a>
 </div>
-
-
-
-
-
-
-
